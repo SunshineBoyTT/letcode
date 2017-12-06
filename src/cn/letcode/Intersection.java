c002fbb7120d806ebef3ac90419bb951c86fc045
PC-20170308MFOG\Administrator <827890676@qq.com>
2017-12-06 15:58:10 +0800

    数组分割

---
+++ src/cn/letcode/Intersection.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package cn.letcode;
+
+/**
+ * @Description:
+ * @author zhengjuntao@hjtechcn.cn
+ * 有两个有序的集合，集合的每个元素都是一段范围，求其交集，例如集合{[4,8],[9,13]}和{[6,12]}的交集为{[6,8],[9,12]}
+ * @Since:2017年12月6日
+ * @Version:1.1.0
+ */
+public class Intersection {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}

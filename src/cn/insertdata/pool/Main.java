63179238b9c9a4e2adf7143eec0ee9d805feda8b
SunshineBoyTT <827890676@qq.com>
2017-12-11 22:17:43 +0800

    线程池修改

--- src/cn/insertdata/pool/Main.java
+++ src/cn/insertdata/pool/Main.java
@@ -5,26 +5,56 @@ package cn.insertdata.pool;
 
 import java.sql.Connection;
 
+import cn.insertdata.pool.ConnectPool.DBConnectionPool;
+
 /**
- * @Description:
+ * @Description:简单实现 :深入请看博客http://www.importnew.com/19011.html
  * @author zhengjuntao@hjtechcn.cn
  * @Since:2017年12月11日
  * @Version:1.1.0
  */
 public class Main {
 	public static void main(String[] args) {
-		PoolMan poolMan=new PoolMan();
+		final PoolMan poolMan=new PoolMan();
 		try {
-			Connection con=null;
 			for (int i = 0; i < 5; i++) {
-				con=poolMan.getPool("app");
-				System.out.println(con.toString());
-				poolMan.close("app");
+//				con=poolMan.getPool("app");
+////				System.out.println(con.toString());
+//				poolMan.close("app");
+				new Thread(){
+					public void run() {
+						try {
+							Connection con=poolMan.getPool("app");
+							System.out.println(con.toString());
+							poolMan.close("app");
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					};
+				}.start();
 			}
 			System.err.println("---------------------------");
+			for (int i = 0; i < 5; i++) {
+//				con=poolMan.getPool("app");
+////				System.out.println(con.toString());
+//				poolMan.close("app");
+				new Thread(){
+					public void run() {
+						try {
+							Connection con=poolMan.getPool("app");
+							System.out.println(con.toString());
+							poolMan.close("app");
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					};
+				}.start();
+			}
+			
+			
 //			Thread.sleep(1000);
-			con=poolMan.getConnection("app");
-			System.out.println(con.toString());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
